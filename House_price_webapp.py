c73122ebebc5f765c08c942b8732b6e21b06e646
Jenitha Lessie S <102992686+JenithaLessie@users.noreply.github.com>
2025-05-16 10:37:48 +0100

    House_price_webapp.py

--- House_price_webapp.py
+++ House_price_webapp.py
@@ -37,7 +37,7 @@ unsafe_allow_html=True,
 st.header("House Price Prediction")
 model=joblib.load("house_price_model.joblib")
 
-col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
           
